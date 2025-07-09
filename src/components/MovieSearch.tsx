99cc03680807d309ace88f12ed2a61723518af42
vukasinvujic <vukasinvujic021@gmail.com>
2025-07-09 11:35:27 +0200

    creating Movie Search component

---
+++ src/components/MovieSearch.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { searchMovies } from "../features/movies/moviesSlice";
+import type { AppDispatch, RootState } from "../store";
+
+const MovieSearch = () => {
+  const [query, setQuery] = useState("");
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const { movies, loading, error } = useSelector(
+    (state: RootState) => state.movies
+  );
+
+  const handleSearch = () => {
+    if (query.trim()) {
+      dispatch(searchMovies(query));
+    }
+  };
+
+  return (
+    <div>
+      <h1>Movi Browser</h1>
+      <input
+        type="text"
+        placeholder="Search movies.."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <button onClick={handleSearch}>Search</button>
+
+      {loading && <p>Loading...</p>}
+      {error && <p style={{ color: "red" }}>Error...</p>}
+
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.imdbID}>
+            <strong> {movie.Title} </strong> ({movie.Year})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MovieSearch;
